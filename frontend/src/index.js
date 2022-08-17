5a0ff8e43785c9da350ba3a1c4dded169d0fcb92
EndritShala30 <endritt1.shala@gmail.com>
2022-08-17 16:16:03 +0200

    new commits

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactDOM from "react-dom";
+import React from "react";
+import { AuthContextProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
